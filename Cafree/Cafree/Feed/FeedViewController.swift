f56e063c56c19ef3042886d5828f5398fd6085d6
한수빈 <realhsb@gmail.com>
2022-11-25 18:42:21 +0900

    chore : FeedVC 틀 구성

--- Cafree/Cafree/Feed/FeedViewController.swift
+++ Cafree/Cafree/Feed/FeedViewController.swift
@@ -8,22 +8,48 @@
 import UIKit
 
 class FeedViewController: ViewController {
+    
+    fileprivate let tableCollectionView:UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        return cv
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(tableCollectionView)
+        tableCollectionView.backgroundColor = .white
+        tableCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 180).isActive = true
+        tableCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
+        tableCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
+        tableCollectionView.heightAnchor.constraint(equalTo: view.heightAnchor, constant: -40).isActive = true
+        
+        tableCollectionView.delegate = self
+        tableCollectionView.dataSource = self
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width - 50, height: collectionView.frame.width - 30)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    
+    
+    
 }
