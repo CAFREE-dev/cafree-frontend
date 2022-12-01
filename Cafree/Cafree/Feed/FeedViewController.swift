20aa77b1295b056af047b923cd57122700cdd3da
한수빈 <realhsb@gmail.com>
2022-12-02 05:44:41 +0900

    chore : FeedVC, Main 스토리보드 수정

--- Cafree/Cafree/Feed/FeedViewController.swift
+++ Cafree/Cafree/Feed/FeedViewController.swift
@@ -1,55 +1,121 @@
-//
-//  FeedViewController.swift
-//  Cafree
-//
-//  Created by 한수빈 on 2022/11/24.
-//
-
 import UIKit
-
-class FeedViewController: ViewController {
+class FeedViewController : UIViewController {
     
-    fileprivate let tableCollectionView:UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        cv.translatesAutoresizingMaskIntoConstraints = false
-        cv.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-        
-        return cv
-    }()
-
+    @IBOutlet weak var sortBtn: UIButton!
+    var isClicked = false
+    
+    @IBOutlet weak var listView: UIView!
+    @IBOutlet weak var gridView: UIView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.addSubview(tableCollectionView)
-        tableCollectionView.backgroundColor = .white
-        tableCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 180).isActive = true
-        tableCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
-        tableCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
-        tableCollectionView.heightAnchor.constraint(equalTo: view.heightAnchor, constant: -40).isActive = true
-        
-        tableCollectionView.delegate = self
-        tableCollectionView.dataSource = self
     }
+    
+    
+    @IBAction func sortBtnClicked(_ sender: UIButton) { // true : list, false : grid
+        if !isClicked {
+            sender.setImage(UIImage(named: "square.grid.2x2.fill"), for:.normal)
+            isClicked = true
+            showGridView()
+        } else if isClicked {
+            sender.setImage(UIImage(named: "pencil.circle.fill"), for: .normal)
+            isClicked = false
+            showListView()
+        }
+    }
+    
+    func showListView(){
+        listView.alpha = 1.0
+        gridView.alpha = 0.0
+    }
+    
+    func showGridView(){
+        listView.alpha = 0.0
+        gridView.alpha = 1.0
+    }
+    
+    
 }
+
 
-extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource{
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width - 50, height: collectionView.frame.width - 30)
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//import UIKit
+//
+//class FeedViewController: ViewController {
+    
+//    lazy var horizontalScrollView: HorizontalScrollView = {
+//        let view = HorizontalScrollView()
+//        return view
+//    }()
+//
+//    fileprivate let tableCollectionView:UICollectionView = {
+//        let layout = UICollectionViewFlowLayout()
+//        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+//        cv.translatesAutoresizingMaskIntoConstraints = false
+//        cv.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+//
+//        return cv
+//    }()
+
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+        
+//        configure()
+//
+//        view.addSubview(tableCollectionView)
+//        tableCollectionView.backgroundColor = .white
+//        tableCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 180).isActive = true
+//        tableCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
+//        tableCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
+//        tableCollectionView.heightAnchor.constraint(equalTo: view.heightAnchor, constant: -40).isActive = true
+//
+//        tableCollectionView.delegate = self
+//        tableCollectionView.dataSource = self
+        
+
+//    }
+    
+//    private func configure(){
+//        view.addSubview(horizontalScrollView)
+//        horizontalScrollView.snp.makeConstraints{ make in
+//            make.center.width.equalToSuperview()
+//            make.height.equalTo(56)
+//        }
+//    }
     
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .red
-        return cell
-    }
-    
-    
-    
-    
-}
+//}
+
+//extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource{
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: collectionView.frame.width - 50, height: collectionView.frame.width - 30)
+//    }
+//
+//
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 3
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+//        cell.backgroundColor = .red
+//        return cell
+//    }
+//}
