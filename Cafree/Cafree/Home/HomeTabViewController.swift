005b43bdc854a89246ba9057278fad61b31bd8f2
eemdeeks <prince9088@gmail.com>
2022-12-04 00:35:08 +0900

    style : 스토리보드 분리

---
+++ Cafree/Cafree/Home/HomeTabViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeTabViewController.swift
+//  Cafree
+//
+//  Created by 박승찬 on 2022/12/04.
+//
+
+import UIKit
+
+class HomeTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
