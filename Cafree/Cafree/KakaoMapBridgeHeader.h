8bd61a49717163965a554d8a4cb8923014184120
eemdeeks <prince9088@gmail.com>
2022-12-04 02:16:21 +0900

    build : 카카오맵 api 빌드 설정

---
+++ Cafree/Cafree/KakaoMapBridgeHeader.h
@@ -0,0 +1,13 @@
+//
+//  KakaoMapBridgeHeader.h
+//  Cafree
+//
+//  Created by 박승찬 on 2022/12/04.
+//
+
+#ifndef KakaoMapBridgeHeader_h
+#define KakaoMapBridgeHeader_h
+
+#import <DaumMap/MTMapView.h>
+
+#endif /* KakaoMapBridgeHeader_h */
