5aaa6bb78fd1c33ad3dd98d7dc00e35c7d13d2a1
eemdeeks <prince9088@gmail.com>
2022-12-05 22:03:21 +0900

    feat : 홈 화면 상단 탭

--- Cafree/Cafree/Home/HomeTabViewController.swift
+++ Cafree/Cafree/Home/HomeTabViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 class HomeTabViewController: UIViewController {
 
     @IBOutlet var btn : UIButton!
+    
+    @IBOutlet weak var popCafeView: UIView!
+    @IBOutlet weak var nearCafeView: UIView!
+    @IBOutlet weak var conseptCafeView: UIView!
+    @IBOutlet weak var newCafeView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,6 +28,30 @@ class HomeTabViewController: UIViewController {
         
         guard let myLocaionView = self.storyboard?.instantiateViewController(identifier: "MyLocationViewController") else {return}
           self.present(myLocaionView, animated: true)
+    }
+    
+    @IBAction func switchView(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            popCafeView.alpha = 1
+            nearCafeView.alpha = 0
+            conseptCafeView.alpha = 0
+            newCafeView.alpha = 0
+        }else if sender.selectedSegmentIndex == 1 {
+            popCafeView.alpha = 0
+            nearCafeView.alpha = 1
+            conseptCafeView.alpha = 0
+            newCafeView.alpha = 0
+        }else if sender.selectedSegmentIndex == 2 {
+            popCafeView.alpha = 0
+            nearCafeView.alpha = 0
+            conseptCafeView.alpha = 1
+            newCafeView.alpha = 0
+        }else {
+            popCafeView.alpha = 0
+            nearCafeView.alpha = 0
+            conseptCafeView.alpha = 0
+            newCafeView.alpha = 1
+        }
     }
     
 }
