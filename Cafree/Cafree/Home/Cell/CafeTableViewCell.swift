b6adaf2b10e22db25e543349da6c2b5b6628e3d4
eemdeeks <prince9088@gmail.com>
2022-12-06 21:35:43 +0900

    feat : 테이블 뷰, sample 소스 추가

---
+++ Cafree/Cafree/Home/Cell/CafeTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  CafeTableViewCell.swift
+//  Cafree
+//
+//  Created by 박승찬 on 2022/12/06.
+//
+
+import UIKit
+
+class CafeTableViewCell: UITableViewCell {
+
+   
+    @IBOutlet var cafeView: UIView!
+    @IBOutlet var cafeImgView: UIImageView!
+    @IBOutlet var cafeName: UILabel!
+    @IBOutlet var cafeInfo: UILabel!
+    @IBOutlet var cafeDist: UILabel!
+    @IBOutlet var cafeLocal: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Cafree/Cafree/Home/PopCafeViewController.swift
+++ Cafree/Cafree/Home/PopCafeViewController.swift
@@ -7,14 +7,20 @@
 
 import UIKit
 
-class PopCafeViewController: UIViewController {
+class PopCafeViewController: UIViewController{
 
     
     @IBOutlet var btnLocation : UIButton!
     
+    @IBOutlet weak var cafeTableView: UITableView!
+    
+    var cafes = ["sample1","sample2"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        cafeTableView.dataSource = self
+        
     }
  
     //버튼 클릭시
@@ -25,4 +31,20 @@ class PopCafeViewController: UIViewController {
         
     }
     
+}
+
+extension PopCafeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cafes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = cafeTableView.dequeueReusableCell(withIdentifier: "cafeCell") as! CafeTableViewCell
+        let cafe = cafes[indexPath.row]
+        
+        cell.cafeName.text = cafe
+        cell.cafeImgView.image = UIImage(named: "sampleCafeImg"+String(indexPath.row+1))
+        
+        return cell
+    }
 }
