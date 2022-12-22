2cb9ac171cc57b1559f7998f385d2b15bb2d1c8f
한수빈 <realhsb@gmail.com>
2022-12-23 04:09:16 +0900

    feat : ListCollectionView 셀 등록

--- Cafree/Cafree/Feed/List/ListViewController.swift
+++ Cafree/Cafree/Feed/List/ListViewController.swift
@@ -9,21 +9,40 @@ import UIKit
 
 class ListViewController: UIViewController {
 
+    @IBOutlet var listViewController: UICollectionView!
+    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        listViewController.delegate = self
+        listViewController.dataSource = self
+        
+        listViewController.reloadData()
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
+extension ListViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource{
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width: CGFloat = collectionView.frame.width - 20
+        let height: CGFloat = collectionView.frame.height - 100
+
+        return CGSize(width: width, height: height)
     }
-    */
+
+
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+            cell.backgroundColor = .red
+            return cell
+    }
+
 
 }
