03c678ab20816b1b5ca65b351f2895b83619f201
한수빈 <realhsb@gmail.com>
2022-12-23 04:11:36 +0900

    chore : List, Grid CollectionViewCell 파일 추가

---
+++ Cafree/Cafree/Feed/Grid/Cell/GridCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  GridCollectionViewCell.swift
+//  Cafree
+//
+//  Created by 한수빈 on 2022/12/23.
+//
+
+import UIKit
+
+class GridCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var cafeImg: UIImageView!
+    
+}
