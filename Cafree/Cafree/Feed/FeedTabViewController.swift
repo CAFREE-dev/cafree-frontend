9fe21bb13fa26d15f78ff97d660e4f8a33a7ba2b
한수빈 <realhsb@gmail.com>
2022-12-26 03:23:11 +0900

    chore : 내비게이션바 색상 초기화

--- Cafree/Cafree/Feed/FeedTabViewController.swift
+++ Cafree/Cafree/Feed/FeedTabViewController.swift
@@ -16,6 +16,7 @@ class FeedTabViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         initTagBtn()
+        initNavigationBarColor()
     }
     
     func initTagBtn(){
@@ -31,6 +32,14 @@ class FeedTabViewController: UIViewController {
 
             tagBtnIndex += 1
         }
+    }
+    
+    func initNavigationBarColor() {
+        // 네비게이션 바 색상 변경
+        let navigationBarAppearance = UINavigationBarAppearance() // navigationBar 를 설정해줘야 스크롤 했을 때 색상이 안 보임.
+        navigationBarAppearance.backgroundColor = .primary
+        self.navigationController?.navigationBar.standardAppearance = navigationBarAppearance
+        self.navigationController?.navigationBar.scrollEdgeAppearance = navigationBarAppearance
     }
     
     
