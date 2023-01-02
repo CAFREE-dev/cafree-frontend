365a4613b016774e5024c3c99b136f143a515078
한수빈 <realhsb@gmail.com>
2023-01-02 19:52:00 +0900

    chore : 프로필 디자인 수정

--- Cafree/Cafree/Profile/ProfileTabViewController.swift
+++ Cafree/Cafree/Profile/ProfileTabViewController.swift
@@ -9,10 +9,52 @@ import UIKit
 
 class ProfileTabViewController: UIViewController {
 
+    // 사용자 프로필 닉네임
+    @IBOutlet var nameLabel: UILabel!
+    
+    // 사용자 프로필 사진
+    @IBOutlet var profileImg: UIImageView!
+    
+    // 게시글, 좋아요 라벨 (고정)
+    @IBOutlet var postLabel: UILabel!
+    @IBOutlet var likeLabel: UILabel!
+    
+    // 게시글, 좋아요 개수
+    @IBOutlet var postCount: UILabel!
+    @IBOutlet var likeCount: UILabel!
+    
+    // 사용자 프로필 수정 버튼
+    @IBOutlet var editBtn: UIButton!
+    
+    // 게시물-좋아요 분류 세그먼트
+    @IBOutlet var sortSegment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         initNavigationBarColor()
+        
+        // 프로필 초기화 
+        initProfile()
+        initEditBtn()
+        
+    }
+    
+    func initProfile() {
+        nameLabel.text = "한수빈"
+
+        postLabel.text = "게시글"
+        likeLabel.text = "좋아요"
+        
+        postCount.text = "10"
+        likeCount.text = "10"
+    }
+    
+    func initEditBtn() {
+        editBtn.setTitle("프로필 수정", for: .normal)
+        editBtn.layer.borderWidth = 0.5
+        editBtn.layer.borderColor = UIColor.secondary?.cgColor
+        editBtn.layer.cornerRadius = 4
     }
     
 
