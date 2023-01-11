654eb5dd8a8fab5a25a0cc61edc51e27e6884ddb
한수빈 <realhsb@gmail.com>
2023-01-11 18:24:55 +0900

    chore : 로그인 화면 UI 구현

--- Cafree/Cafree/Signup/SignUpViewController.swift
+++ Cafree/Cafree/Signup/SignUpViewController.swift
@@ -15,15 +15,22 @@ class SignUpViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginK(_ sender: UIButton) {
+        
+        let mainNC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainTabBarViewController")
+            mainNC.modalPresentationStyle = .fullScreen
+            mainNC.modalTransitionStyle = .crossDissolve
+            present(mainNC, animated: true)
+        
     }
-    */
-
+    
+   
 }
+
+
+//func showMainViewController() {
+//    let mainNC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainTapBarController")
+//    mainNC.modalPresentationStyle = .fullScreen
+//    mainNC.modalTransitionStyle = .crossDissolve
+//    present(mainNC, animated: true)
+//}
