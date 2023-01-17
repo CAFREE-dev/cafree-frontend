8b0e22f7f68f429c73f90026988afe3a5c6ac1f3
eemdeeks <prince9088@gmail.com>
2023-01-17 22:59:41 +0900

    chore : 클립보드 복사시 토스트 메시지 띄우기

--- Cafree/Cafree/Home/DetailCafeViewController.swift
+++ Cafree/Cafree/Home/DetailCafeViewController.swift
@@ -139,7 +139,7 @@ class DetailCafeViewController: UIViewController, UITableViewDelegate, UITableVi
             
             
             cell.backgroundColor = UIColor.clear.withAlphaComponent(0)
-            
+            cell.delegate = self
             return cell
         }
     }
@@ -192,9 +192,30 @@ class DetailCafeViewController: UIViewController, UITableViewDelegate, UITableVi
         guard let writeView = self.storyboard?.instantiateViewController(identifier: "WriteViewController") else {return}
         self.present(writeView, animated: true)
     }
-    
+
+    func showToast(message : String, font: UIFont = UIFont.systemFont(ofSize: 14.0)) {
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 100, y: self.view.frame.size.height-100, width: 200, height: 30))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.font = font
+        toastLabel.textAlignment = .center;
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 15;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 10.0, delay: 0.1, options: .curveEaseOut, animations: {
+             toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
 }
-extension DetailCafeViewController: ComponentProductCellDelegate{
+
+extension DetailCafeViewController: ComponentProductCellDelegate, CafeDetailLocalTableViewCellDelegate{
+    func copyAdress() {
+        self.showToast(message: "클립보드에 복사되었습니다.")
+    }
     
     func selectedGoodBtn(goodIndex: Int) {
         if goodIndex == 0{
